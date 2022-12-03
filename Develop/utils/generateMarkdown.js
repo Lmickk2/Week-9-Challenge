84041c068f68b914beece9fb978a3b9149ed9b64
Lmickk2 <lukemickan21@icloud.com>
2022-12-03 16:50:41 -0500

    removed attempts for license badge function

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -29,32 +29,6 @@ const generateMarkdown=(response) => {
 `
 }
 
-// return 
-// function renderLicenseBadge(license) {
-
-// }
-
-// // TODO: Create a function that returns the license link
-// // If there is no license, return an empty string
-// function renderLicenseLink(license) {
-//   if(!license) {
-//     return `https://img.shields.io/badge/license-${license}-blue`
-//   }
-//   else {
-//     return ' '
-//   }
-// }
-
-// // TODO: Create a function that returns the license section of README
-// // If there is no license, return an empty string
-// function renderLicenseSection(license) {
-//   if(!license) {
-//     return `https://choosealicense.com/licenses/${license}`
-//   }
-//   else {
-//     return ' '
-//   }
-// }
 
 // TODO: Create a function to generate markdown for README
 module.exports=generateMarkdown
